245eb604ef2763210f7b4e953b9c87d553d64b10
David Vodnik <davidvodnik@gmail.com>
2022-11-07 16:50:31 +0100

    Create sdl window

--- src/pond.cc
+++ src/pond.cc
@@ -1,2 +1,22 @@
+#include "SDL.h"
+#include "SDL_events.h"
+#include "SDL_video.h"
 
-int main() { return 0; }
+int main() {
+    SDL_Init(SDL_INIT_VIDEO);
+    auto window = SDL_CreateWindow("Pond", 0, 0, 640, 480, 0);
+    auto surface = SDL_GetWindowSurface(window);
+
+    while (true) {
+        SDL_Event e;
+        while (SDL_PollEvent(&e)) {
+            switch (e.type) {
+            case SDL_QUIT:
+                return 0;
+            }
+        }
+        SDL_UpdateWindowSurface(window);
+    }
+
+    return 0;
+}
