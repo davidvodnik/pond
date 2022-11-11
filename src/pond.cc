4201a047d32a55aef099a38e0d0c1dd7a748974d
David Vodnik <davidvodnik@gmail.com>
2022-11-12 00:10:48 +0100

    Add basic opengl window

--- src/pond.cc
+++ src/pond.cc
@@ -1,11 +1,17 @@
+#include "GL/glew.h"
 #include "SDL.h"
 #include "SDL_events.h"
 #include "SDL_video.h"
 
 int main() {
     SDL_Init(SDL_INIT_VIDEO);
-    auto window = SDL_CreateWindow("Pond", 0, 0, 640, 480, 0);
-    auto surface = SDL_GetWindowSurface(window);
+    auto window = SDL_CreateWindow("Pond", 0, 0, 640, 480,
+                                   SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+    SDL_GL_CreateContext(window);
+
+    glewInit();
+
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 
     while (true) {
         SDL_Event e;
@@ -15,7 +21,9 @@ int main() {
                 return 0;
             }
         }
-        SDL_UpdateWindowSurface(window);
+
+        glClear(GL_COLOR_BUFFER_BIT);
+        SDL_GL_SwapWindow(window);
     }
 
     return 0;
